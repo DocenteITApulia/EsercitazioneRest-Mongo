1d844a420f01f5163b66e22e12f936b9d785bb2f
DocenteITApulia <docentedev2022mf@studenti.apuliadigitalmaker.it>
2021-09-23 06:32:06 +0200

    aggiunta link utili

--- src/main/java/it/apulia/EsercitazioneRestMongo/EsercitazioneRestMongoApplication.java
+++ src/main/java/it/apulia/EsercitazioneRestMongo/EsercitazioneRestMongoApplication.java
@@ -10,6 +10,14 @@ public class EsercitazioneRestMongoApplication {
 		SpringApplication.run(EsercitazioneRestMongoApplication.class, args);
 	}
 
+/* Link per gestione logiche e errori
+* https://stackabuse.com/how-to-return-http-status-codes-in-a-spring-boot-application/
+* https://www.amitph.com/spring-return-specific-http-status/
+* https://www.baeldung.com/spring-mvc-controller-custom-http-status-code --> soluzioni rapide (solo response entity o custom exception)
+* https://www.baeldung.com/exception-handling-for-rest-with-spring
+* 	--> esempio soluzione 4 con custom exception
+* 	--> esempio soluzione 3 potrebbe utile se lanciamo nei servizi un certo tipo di eccezione che verrà raccolta da questa classe
+*/
 
 
 }
